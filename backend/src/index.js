0019eaffe2f1d761b0890daf08a51e58e018c407
Nilesh Raut <nileshraut898@gmail.com>
2025-11-21 20:17:04 +0530

    minor fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -40,7 +40,7 @@ app.use(
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 
-const frontendBuildPath = path.join(__dirname, '../../frontend-cra/build');
+const frontendBuildPath = path.join(__dirname, '../../frontend/build');
 app.use(express.static(frontendBuildPath));
 
 app.get('/healthz', healthCheck);
